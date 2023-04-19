2c12a67c760b83ec00f1d56b34d7b12024275c40
anisul islam <anisulislam980340@gmail.com>
2023-04-19 07:01:31 +0600

    string concatinate

---
+++ built-in-string-compare.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+  char a[100], b[100]        ;
+  scanf("%s %s", a, b);
+  int v= strcmp(a,b);
+  if(v<0){
+    printf("A choto");
+  }
+  else if(v>0){
+    printf("B choto");
+  }
+  else{
+    printf("same");
+  }
+  return 0;
+}
---
+++ string-compare.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main(){
+  char a[100], b[100];
+  scanf("%s %s", a, b);
+  int i=0;
+  while(1){
+    if(a[i]=='\0' && b[i]=='\0'){
+        printf("same\n");
+        break;
+    }
+    else if(a[i]=='\0'){
+        printf("A choto");
+        break;
+    }
+    else if( b[i]=='\0'){
+        printf("B choto");
+        break;
+    }
+
+
+    if(a[i]==b[i]){
+        i++;
+    }
+    else if( a[i]< b[i]){
+        printf( "a choto");
+        break;
+    }
+    else{
+        printf("b choto");
+        break;
+    }
+
+
+  }
+  return 0;
+}
